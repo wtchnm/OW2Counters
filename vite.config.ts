69134fef6eeef6dfa651ecb0ed34a495ec619179
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2026-03-13 22:40:14 -0300

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import {defineConfig} from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-	plugins: [solid(), tailwindcss()],
-	build: {target: 'esnext'}
+	plugins: [solid(), tailwindcss()]
 })
